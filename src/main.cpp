632babd5bbbae69e14f6eb95cf61a93b6c042885
annie <riverknuuttila2@outlook.com>
2026-03-16 19:41:39 -0400

    added scoreboard names & fixed bug where win condition would write second place

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 
-  //2048 clone in the linux terminal.
-  //2048 clone Copyright (c) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-  //author: River Knuuttila (common alias: Annie, Ann, Annie Knuuttila)
+ //2048 clone in the linux terminal.
+ //2048 clone Copyright (c) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ //author: River Knuuttila (common alias: Annie, Ann, Annie Knuuttila)
 
-  //This program is free software: you can redistribute it and/or modify
-  //it under the terms of the GNU General Public License as published by
-  //the Free Software Foundation, either version 3 of the License, or
-  //(at your option) any later version.
+ //This program is free software: you can redistribute it and/or modify
+ //it under the terms of the GNU General Public License as published by
+ //the Free Software Foundation, either version 3 of the License, or
+ //(at your option) any later version.
 
-  //This program is distributed in the hope that it will be useful,
-  //but WITHOUT ANY WARRANTY; without even the implied warranty of
-  //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  //GNU General Public License for more details.
+ //This program is distributed in the hope that it will be useful,
+ //but WITHOUT ANY WARRANTY; without even the implied warranty of
+ //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ //GNU General Public License for more details.
 
-  //You should have received a copy of the GNU General Public License
-  //along with this program.  If not, see <https://www.gnu.org/licenses/>.
-  
-  //Concerns or questions? Reach out at: riverknuuttila2@outlook.com
+ //You should have received a copy of the GNU General Public License
+ //along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ //Concerns or questions? Reach out at: riverknuuttila2@outlook.com
 
 #include <thread>
 #include <chrono>
@@ -28,7 +28,7 @@
 #include "./include/sysInfo.hpp"
 #include "./include/functions.hpp"
 #include "../libs/inih/INIReader.h"
-  
+
 string tileColor(int value) {
   switch (value) {
     case 0:    return "\033[48;5;187m"; // empty (#cdc1b4)
@@ -74,29 +74,38 @@ void printBox(int displayNumber) {
   }
 }
 
-void scoreCheck(int score, int& first, int& second, int& third, const string& iniPath) {
+int scoreCheck(int score, int& first, int& second, int& third,
+               const string& firstName, const string& secondName, const string& thirdName,
+               const string& iniPath) {
+  int pos = 0;
   if (score > first) {
-    first = score;
+    first = score; pos = 1;
   } else if (score > second) {
-    second = score;
+    second = score; pos = 2;
   } else if (score > third) {
-    third = score;
+    third = score; pos = 3;
   } else {
-    return;
+    return 0;
   }
   ofstream ini(iniPath);
   ini << "[leaderBoard]\n"
-    << "first="  << first  << "\n"
-    << "second=" << second << "\n"
-    << "third="  << third  << "\n";
+    << "first="      << first      << "\n"
+    << "firstName="  << firstName  << "\n"
+    << "second="     << second     << "\n"
+    << "secondName=" << secondName << "\n"
+    << "third="      << third      << "\n"
+    << "thirdName="  << thirdName  << "\n";
+  return pos;
 }
 
-void printGame(int playingGrid[4][4], int& first, int& second, int& third, int score) {
+void printGame(int playingGrid[4][4], int& first, int& second, int& third,
+               const string& firstName, const string& secondName, const string& thirdName,
+               int score) {
   bool isFirst  = score > 0 && score == first;
   bool isSecond = !isFirst && score > 0 && score == second;
   bool isThird  = !isFirst && !isSecond && score > 0 && score == third;
 
-  const int W = 20;
+  const int W = 28;
   auto visLen = [](const string& s) -> int {
     int n = 0; bool esc = false;
     for (char c : s) {
@@ -106,23 +115,25 @@ void printGame(int playingGrid[4][4], int& first, int& second, int& third, int s
     }
     return n;
   };
-  auto makeLabel = [&](const string& prefix, int val, const string& suffix = "") -> string {
+  auto makeLabel = [&](const string& prefix, int val, const string& name, const string& suffix = "") -> string {
     string valStr = to_string(val);
+    string nameStr = name.size() > 12 ? name.substr(0, 12) : name;
     string s = prefix + valStr;
-    int vis = visLen(prefix) + (int)valStr.size();
+    if (!nameStr.empty()) s += " " + nameStr;
+    int vis = visLen(prefix) + (int)valStr.size() + (nameStr.empty() ? 0 : 1 + (int)nameStr.size());
     while (vis++ < W) s += ' ';
     s += suffix;
     return s;
   };
 
   string labels[4] = {
-    makeLabel(isFirst  ? "\033[48;5;209m\033[30m Current 1st: " : " 1st: ",  first,  isFirst  ? "\033[0m" : ""),
-    makeLabel(isSecond ? "\033[48;5;220m\033[30m Current 2nd: " : " 2nd: ", second, isSecond ? "\033[0m" : ""),
-    makeLabel(isThird  ? "\033[48;5;230m\033[30m Current 3rd: " : " 3rd: ",  third,  isThird  ? "\033[0m" : ""),
-    (isFirst || isSecond || isThird) ? string(W, ' ') : makeLabel("\033[48;5;230m\033[30m Score: ", score, "\033[0m")
+    makeLabel(isFirst  ? "\033[48;5;209m\033[30m Current 1st: " : " 1st: ",  first,  firstName,  isFirst  ? "\033[0m" : ""),
+    makeLabel(isSecond ? "\033[48;5;220m\033[30m Current 2nd: " : " 2nd: ", second, secondName, isSecond ? "\033[0m" : ""),
+    makeLabel(isThird  ? "\033[48;5;230m\033[30m Current 3rd: " : " 3rd: ",  third,  thirdName,  isThird  ? "\033[0m" : ""),
+    (isFirst || isSecond || isThird) ? string(W, ' ') : makeLabel("\033[48;5;230m\033[30m Score: ", score, "", "\033[0m")
   };
 
-  const string hbar  = "────────────────────";
+  const string hbar  = "────────────────────────────";
   const string blank(W, ' ');
 
   cout << "┌────┬────┬────┬────┐" << hbar << "┐" << endl;
@@ -136,7 +147,7 @@ void printGame(int playingGrid[4][4], int& first, int& second, int& third, int s
       cout << "├────┼────┼────┼────┤" << blank << "│" << endl;
     }
   }
-  cout << "└────┴────┴────┴────┘" << hbar << "┘";
+  cout << "└────┴────┴────┴────┘" << hbar << "┘" << endl;
 }
 
 int main() {
@@ -144,7 +155,7 @@ int main() {
   bool won = false;
 
   INIReader reader("../usr/leaderBoard.ini");
-  
+
   if (reader.ParseError() < 0) {
     cout<<"failed to load leaderBoard\n";
   }
@@ -153,11 +164,15 @@ int main() {
   int lbSecond = reader.GetInteger("leaderBoard", "second", 0);
   int lbThird  = reader.GetInteger("leaderBoard", "third",  0);
 
+  string lbFirstName  = reader.Get("leaderBoard", "firstName",  "");
+  string lbSecondName = reader.Get("leaderBoard", "secondName", "");
+  string lbThirdName  = reader.Get("leaderBoard", "thirdName",  "");
+
   int score = 0;
-  
+
   ifstream file("../assets/copywriteNotice.txt");
-    
-  if (!file) { 
+
+  if (!file) {
     cerr<<"Could not open the file!"<<endl;
     return 1;
   }
@@ -174,7 +189,7 @@ int main() {
   cin>>noticeA;
 
 if (noticeA=="r") {
-  
+
   //cout << "Enable RAM readings? (y/n): ";
   //string ramToggle;
   //cin >> ramToggle;
@@ -188,17 +203,18 @@ if (noticeA=="r") {
   getScore(playingGrid, score);
 
   clearScreen();
-    printGame(playingGrid, lbFirst, lbSecond, lbThird, score);
+    printGame(playingGrid, lbFirst, lbSecond, lbThird, lbFirstName, lbSecondName, lbThirdName, score);
     //printSysInfo(ramEnabled);
 
   setBufferedInput(false);
+
+  int bestPosition = 0;
 
   while (true) { //main loop.
     char cont = getchar();
 
     if (cont == 'q') {
-      cout << endl;
-        scoreCheck(score, lbFirst, lbSecond, lbThird, "../usr/leaderBoard.ini");
+      scoreCheck(score, lbFirst, lbSecond, lbThird, lbFirstName, lbSecondName, lbThirdName, "../usr/leaderBoard.ini");
       break;
     }
 
@@ -226,15 +242,35 @@ if (noticeA=="r") {
 
     if (moved) {
       newRandomBox(playingGrid);
-        clearScreen();
           getScore(playingGrid, score);
-          scoreCheck(score, lbFirst, lbSecond, lbThird, "../usr/leaderBoard.ini");
-          printGame(playingGrid, lbFirst, lbSecond, lbThird, score);
+          int pos = scoreCheck(score, lbFirst, lbSecond, lbThird, lbFirstName, lbSecondName, lbThirdName, "../usr/leaderBoard.ini");
+          if (pos > 0 && (bestPosition == 0 || pos < bestPosition)) {
+            bestPosition = pos;
+            setBufferedInput(true);
+            cout << endl << "You reached #" << pos << "! Enter your name: ";
+            string playerName;
+            cin >> playerName;
+            if (pos == 1) lbFirstName = playerName;
+            else if (pos == 2) lbSecondName = playerName;
+            else lbThirdName = playerName;
+            ofstream ini("../usr/leaderBoard.ini");
+            ini << "[leaderBoard]\n"
+              << "first="      << lbFirst      << "\n"
+              << "firstName="  << lbFirstName  << "\n"
+              << "second="     << lbSecond     << "\n"
+              << "secondName=" << lbSecondName << "\n"
+              << "third="      << lbThird      << "\n"
+              << "thirdName="  << lbThirdName  << "\n";
+            setBufferedInput(false);
+          }
+          clearScreen();
+          printGame(playingGrid, lbFirst, lbSecond, lbThird, lbFirstName, lbSecondName, lbThirdName, score);
           //printSysInfo(ramEnabled);
 
       if (!canMove(playingGrid)) { //lose condition.
-        cout<< endl <<"Game Over!" << endl << "score: "<< score << endl;
-        scoreCheck(score, lbFirst, lbSecond, lbThird, "../usr/leaderBoard.ini");
+        cout <<"Game Over!" << endl << "score: "<< score << endl;
+        int pos2 = scoreCheck(score, lbFirst, lbSecond, lbThird, lbFirstName, lbSecondName, lbThirdName, "../usr/leaderBoard.ini");
+        if (pos2 > 0 && (bestPosition == 0 || pos2 < bestPosition)) bestPosition = pos2;
         break;
       }
     }
@@ -242,11 +278,8 @@ if (noticeA=="r") {
     for (int i = 0; i < 4; i++) {
       for (int j = 0; j<4; j++) {
         if (playingGrid[i][j]==2048 && !won) { //win condition
-          getScore(playingGrid,score);
           cout<<endl;
-
-          cout<<"Congrats! You win! Keep playing for a higher score or press 'q' to quit."<<endl<<"score : "<<score<<endl; 
-          scoreCheck(score, lbFirst, lbSecond, lbThird, "../usr/leaderBoard.ini");
+          cout<<"Congrats! You win! Keep playing for a higher score or press 'q' to quit."<<endl<<"score : "<<score<<endl;
           std::this_thread::sleep_for(std::chrono::seconds(2));
           won = true;
         }
@@ -255,12 +288,14 @@ if (noticeA=="r") {
   } //End of main game loop.
 
   setBufferedInput(true);
+
 }
+
 else if (noticeA=="d") {
-  
+
   ifstream file("../LICENSE");
-  
-  if (!file) { 
+
+  if (!file) {
     cerr<<"Could not open the file!"<<endl;
     return 1;
   }
